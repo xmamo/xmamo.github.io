21c53e1cdedd69bea2915780a24be2292e21c483
Matteo Morena <morena.matteo@gmail.com>
2019-06-13 16:47:14 +0200

    Make a and d changeable in BiLoMa

--- js/projects/biloma.js
+++ js/projects/biloma.js
@@ -13,9 +13,6 @@ var MathJax = {
 		return Math.floor(random(min, max));
 	}
 
-	var a = -0.04;
-	var d = 0.98;
-
 	var decimal = "(-?(?:[0-9]+(?:.[0-9]*)?|.[0-9]+))";
 	var delimiter = "(?:\\s*,\\s*|\\s+)";
 	var singleDecimal = "\\s*" + decimal + "\\s*";
@@ -29,15 +26,19 @@ var MathJax = {
 	form["source-pos"].pattern = threeDecimalsPattern;
 	form["destination-pos"].value = randomInt(-64, 64) + ", " + randomInt(64, 128) + ", " + randomInt(-64, 64);
 	form["destination-pos"].pattern = threeDecimalsPattern;
-	form["fly-time"].value = randomInt(1, 6) * 20;
-	form["fly-time"].pattern = singleDecimalPattern;
+	form["air-time"].value = randomInt(1, 6) * 20;
+	form["air-time"].pattern = singleDecimalPattern;
+	form.acceleration.pattern = singleDecimalPattern;
+	form.damping.pattern = singleDecimalPattern;
 
 	form.oninput = function () {
 		var sourcePos = form["source-pos"].value.match(threeDecimalsPattern);
 		var destinationPos = form["destination-pos"].value.match(threeDecimalsPattern);
-		var flyTime = form["fly-time"].value.match(singleDecimalPattern);
+		var airTime = form["air-time"].value.match(singleDecimalPattern);
+		var acceleration = form.acceleration.value.match(singleDecimalPattern);
+		var damping = form.damping.value.match(singleDecimalPattern);
 
-		if (sourcePos === null || destinationPos === null || flyTime === null) {
+		if (sourcePos === null || destinationPos === null || airTime === null || acceleration == null || damping == null) {
 			return false;
 		}
 
@@ -52,19 +53,21 @@ var MathJax = {
 			z: parseFloat(destinationPos[3])
 		};
 
-		var n = parseFloat(flyTime[1]);
+		var n = parseFloat(airTime[1]);
 		var pn = {
 			x: destinationPos.x - sourcePos.x,
 			y: destinationPos.y - sourcePos.y,
 			z: destinationPos.z - sourcePos.z
 		};
+		var a = parseFloat(acceleration[1]);
+		var d = parseFloat(damping[1]);
 
 		var v0;
-		if (d != 1) {
+		if (d == 1) {
 			v0 = {
-				x: pn.x / ((1 - Math.pow(d, n)) / (1 - d)),
-				y: (pn.y - (n - (1 - Math.pow(d, n)) / (1 - d) * d) / (1 - d) * a) / ((1 - Math.pow(d, n)) / (1 - d)),
-				z: pn.z / ((1 - Math.pow(d, n)) / (1 - d))
+				x: pn.x / n,
+				y: (pn.y - n * a) / n,
+				z: pn.z / n
 			};
 		} else {
 			v0 = {
