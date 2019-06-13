b11cbb06d4215e10075f0a72c5c1038ebc379de2
Matteo Morena <morena.matteo@gmail.com>
2019-06-13 08:41:47 +0200

    Use JS compatible with older browsers

--- js/github-download-link-generator.js
+++ js/github-download-link-generator.js
@@ -1,13 +1,18 @@
 "use strict";
 
-for (let element of document.querySelectorAll("[data-github-repo]")) {
-	let httpRequest = new XMLHttpRequest();
-	httpRequest.onreadystatechange = function () {
-		if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
-			let response = JSON.parse(httpRequest.responseText);
-			element.href = response.assets[0].browser_download_url;
-		}
-	};
-	httpRequest.open("GET", `https://api.github.com/repos/${element.getAttribute("data-github-repo")}/releases/latest`);
-	httpRequest.send();
-}
+(function () {
+	var elements = document.querySelectorAll("[data-github-repo]");
+	for (var i = 0; i < elements.length; i++) {
+		(function (element) {
+			var httpRequest = new XMLHttpRequest();
+			httpRequest.onreadystatechange = function () {
+				if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
+					var response = JSON.parse(httpRequest.responseText);
+					element.href = response.assets[0].browser_download_url;
+				}
+			};
+			httpRequest.open("GET", "https://api.github.com/repos/" + element.getAttribute("data-github-repo") + "/releases/latest");
+			httpRequest.send();
+		})(elements[i]);
+	}
+})();
