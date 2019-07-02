430145ee87fe803bd35799a09e2045ad37bbdbe9
Matteo Morena <8678043+xmamo@users.noreply.github.com>
2019-07-02 13:02:43 +0200

    Check for JS with <noscript> instead of using a script (CSS .js-only)

--- js/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-(function () {
-    var elements = document.getElementsByClassName("js-only");
-    while (elements.length > 0) {
-        elements[0].classList.remove("js-only");
-    }
-})();
