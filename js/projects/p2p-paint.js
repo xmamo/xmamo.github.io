37e892df063510022788f2359292184751eb4440
Matteo Morena <8678043+xmamo@users.noreply.github.com>
2019-07-22 11:02:37 +0200

    Make Paint PTP support more than two peers

--- js/projects/p2p-paint.js
+++ js/projects/p2p-paint.js
@@ -2,47 +2,48 @@
 
 (function () {
 	var canvas = document.getElementById("canvas");
-	/** @type {CanvasRenderingContext2D} */ var context = canvas.getContext("2d");
+	var context = canvas.getContext("2d");
+	var onMouseMove = line;
 
 	canvas.width = canvas.clientWidth;
 	canvas.height = canvas.clientHeight;
 
 	var peer = new Peer(null, { debug: 2 });
-	var connection = null;
 
 	peer.on("open", function (id) {
 		document.getElementById("id").innerHTML = "ID: " + id;
 	});
 
-	if (window.location.hash != "") { // Client
-		connection = peer.connect(window.location.hash.slice(1, window.location.hash.length));
+	if (window.location.hash) { // Client
+		var connection = peer.connect(window.location.hash.slice(1, window.location.hash.length));
 
 		connection.on("data", function (data) {
-			if ("data" in data) {
-				context.putImageData(new ImageData(new Uint8ClampedArray(data.data), data.width, data.height), 0, 0);
-			} else {
-				line("#00F", data.x0, data.y0, data.x1, data.y1);
-			}
+			line(data.x0, data.y0, data.x1, data.y1);
 		});
 
-		connection.on("open", function () {
-			var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-			connection.send({ width: imageData.width, height: imageData.height, data: imageData.data });
-		});
+		onMouseMove = function (x0, y0, x1, y1) {
+			line(x0, y0, x1, y1);
+			connection.send({ x0: x0, y0: y0, x1: x1, y1: y1 });
+		};
 	} else { // Server
-		peer.on("connection", function (c) {
-			connection = c;
+		var connections = [];
+
+		peer.on("connection", function (connection) {
+			connections.push(connection);
 
 			connection.on("data", function (data) {
-				if (!("data" in data)) {
-					line("#F00", data.x0, data.y0, data.x1, data.y1);
+				line(data.x0, data.y0, data.x1, data.y1);
+				for (var i = 0; i < connections.length; i++) {
+					if (connections[i] != connection) {
+						connections[i].send(data);
+					}
 				}
 			});
 
-			connection.on("open", function () {
-				var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-				connection.send({ width: imageData.width, height: imageData.height, data: imageData.data });
-			});
+			onMouseMove = function (x0, y0, x1, y1) {
+				line(x0, y0, x1, y1);
+				connection.send({ x0: x0, y0: y0, x1: x1, y1: y1 });
+			};
 		});
 	}
 
@@ -55,14 +56,7 @@
 		var newMouseY = event.clientY - canvas.getBoundingClientRect().y;
 
 		if (mouseDown) {
-			if (window.location.hash != "") { // Client
-				line("#F00", mouseX, mouseY, newMouseX, newMouseY);
-			} else { // Server
-				line("#00F", mouseX, mouseY, newMouseX, newMouseY);
-			}
-			if (connection != null) {
-				connection.send({ x0: mouseX, y0: mouseY, x1: newMouseX, y1: newMouseY });
-			}
+			onMouseMove(mouseX, mouseY, newMouseX, newMouseY);
 		}
 
 		mouseX = newMouseX;
@@ -90,8 +84,7 @@
 		}
 	});
 
-	function line(style, x0, y0, x1, y1) {
-		context.strokeStyle = style;
+	function line(x0, y0, x1, y1) {
 		context.lineWidth = 5;
 		context.lineCap = "round";
 		context.beginPath();
