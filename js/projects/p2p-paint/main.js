daca028325b96741789b8d30f676d3087e2c2f3a
Matteo Morena <8678043+xmamo@users.noreply.github.com>
2019-07-23 10:39:53 +0200

    Make P2P Paint brush size configurable

--- js/projects/p2p-paint/main.js
+++ js/projects/p2p-paint/main.js
@@ -2,8 +2,10 @@
 
 var p2pPaint = p2pPaint || {};
 
-p2pPaint.canvas.width = p2pPaint.canvas.clientWidth;
-p2pPaint.canvas.height = p2pPaint.canvas.clientHeight;
+for (var i = 0; i < p2pPaint.canvas.length; i++) {
+	p2pPaint.canvas[i].width = p2pPaint.canvas[i].clientWidth;
+	p2pPaint.canvas[i].height = p2pPaint.canvas[i].clientHeight;
+}
 
 if (window.location.hash) { // Client
 	p2pPaint.startClient(window.location.hash.slice(1));
