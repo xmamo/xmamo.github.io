0d13374a688459630a75cd22e64a706cca092066
Matteo Morena <8678043+xmamo@users.noreply.github.com>
2019-07-23 11:08:48 +0200

    Add P2P paint link to make it easier for clients to connect

--- js/projects/p2p-paint/main.js
+++ js/projects/p2p-paint/main.js
@@ -2,19 +2,25 @@
 
 var p2pPaint = p2pPaint || {};
 
-for (var i = 0; i < p2pPaint.canvas.length; i++) {
-	p2pPaint.canvas[i].width = p2pPaint.canvas[i].clientWidth;
-	p2pPaint.canvas[i].height = p2pPaint.canvas[i].clientHeight;
-}
+(function () {
+	for (var i = 0; i < p2pPaint.canvas.length; i++) {
+		p2pPaint.canvas[i].width = p2pPaint.canvas[i].clientWidth;
+		p2pPaint.canvas[i].height = p2pPaint.canvas[i].clientHeight;
+	}
 
-p2pPaint.context[0].fillStyle = "#FFF";
-p2pPaint.context[0].fillRect(0, 0, p2pPaint.canvas[0].width, p2pPaint.canvas[0].height);
+	p2pPaint.context[0].fillStyle = "#FFF";
+	p2pPaint.context[0].fillRect(0, 0, p2pPaint.canvas[0].width, p2pPaint.canvas[0].height);
 
-if (window.location.hash) { // Client
-	p2pPaint.startClient(window.location.hash.slice(1));
-} else { // Server
-	p2pPaint.startServer(function (id) {
-		document.getElementById("p2p-paint-id").innerHTML = "ID: " + id;
-		p2pPaint.startClient(id);
-	});
-}
+	var params = new URLSearchParams(window.location.search);
+
+	if (params.has("remote")) {
+		p2pPaint.startClient(params.get("remote"));
+	} else {
+		p2pPaint.startServer(function (id) {
+			params.set("remote", id);
+			var link = "/projects/p2p-paint/?" + params;
+			document.getElementById("p2p-paint-id").innerHTML = 'Your friends should connect to: <a href="' + link + '">' + window.location.origin + link + '</a>';
+			p2pPaint.startClient(id);
+		});
+	}
+})();
