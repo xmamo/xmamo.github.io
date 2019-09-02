1f93c04feb468634771dc78f45760f1e1ba33aa5
Matteo Morena <8678043+xmamo@users.noreply.github.com>
2019-09-02 12:08:50 +0200

    Optimize and reaorganize renderer

--- js/fun/game-of-life-3d/renderer.js
+++ js/fun/game-of-life-3d/renderer.js
@@ -7,10 +7,10 @@ gameOfLife3d.Renderer = function (gl, world) {
 	var positions = [];
 	var normals = [];
 	var indices = [];
-	var array = [{ positions: positions, normals: normals, indices: indices }];
+	var arrays = [{ positions: positions, normals: normals, indices: indices }];
 	var io = 0; // Index offset
 	var updated = 0;
-	var buffersArray = [];
+	var buffers = [];
 
 	self.world = world;
 	self.onUpdateComplete = function () { };
@@ -155,16 +155,16 @@ gameOfLife3d.Renderer = function (gl, world) {
 					positions = [];
 					normals = [];
 					indices = [];
-					array.push({ positions: positions, normals: normals, indices: indices });
+					arrays.push({ positions: positions, normals: normals, indices: indices });
 					io = 0;
 				}
 			}
 
 			if (++updated === self.world.volume) {
-				var arrayLength = array.length;
+				var arrayLength = arrays.length;
 
-				while (buffersArray.length < arrayLength) {
-					buffersArray.push({
+				while (buffers.length < arrayLength) {
+					buffers.push({
 						positionsBuffer: gl.createBuffer(),
 						normalsBuffer: gl.createBuffer(),
 						indicesBuffer: gl.createBuffer(),
@@ -173,22 +173,25 @@ gameOfLife3d.Renderer = function (gl, world) {
 				}
 
 				for (var i = 0; i < arrayLength; i++) {
-					gl.bindBuffer(gl.ARRAY_BUFFER, buffersArray[i].positionsBuffer);
-					gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(i < arrayLength ? array[i].positions : []), gl.DYNAMIC_DRAW);
+					var buffer = buffers[i];
+					var array = arrays[i];
 
-					gl.bindBuffer(gl.ARRAY_BUFFER, buffersArray[i].normalsBuffer);
-					gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(i < arrayLength ? array[i].normals : []), gl.DYNAMIC_DRAW);
+					gl.bindBuffer(gl.ARRAY_BUFFER, buffer.positionsBuffer);
+					gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(array.positions), gl.DYNAMIC_DRAW);
 
-					gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffersArray[i].indicesBuffer);
-					gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(i < arrayLength ? array[i].indices : []), gl.DYNAMIC_DRAW);
+					gl.bindBuffer(gl.ARRAY_BUFFER, buffer.normalsBuffer);
+					gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(array.normals), gl.DYNAMIC_DRAW);
 
-					buffersArray[i].count = i < arrayLength ? array[i].indices.length : 0;
+					gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffer.indicesBuffer);
+					gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(array.indices), gl.DYNAMIC_DRAW);
+
+					buffers[i].count = array.indices.length;
 				}
 
 				positions = [];
 				normals = [];
 				indices = [];
-				array = [{ positions: positions, normals: normals, indices: indices }];
+				arrays = [{ positions: positions, normals: normals, indices: indices }];
 				io = 0;
 				updated = 0;
 
@@ -201,16 +204,16 @@ gameOfLife3d.Renderer = function (gl, world) {
 		gl.enableVertexAttribArray(aPositionLocation);
 		gl.enableVertexAttribArray(aNormalLocation);
 
-		for (var i = 0, buffersArrayLength = buffersArray.length; i < buffersArrayLength; i++) {
-			gl.bindBuffer(gl.ARRAY_BUFFER, buffersArray[i].positionsBuffer);
+		for (var i = 0, buffersArrayLength = buffers.length; i < buffersArrayLength; i++) {
+			gl.bindBuffer(gl.ARRAY_BUFFER, buffers[i].positionsBuffer);
 			gl.vertexAttribPointer(aPositionLocation, 3, gl.FLOAT, false, 0, 0);
 
-			gl.bindBuffer(gl.ARRAY_BUFFER, buffersArray[i].normalsBuffer);
+			gl.bindBuffer(gl.ARRAY_BUFFER, buffers[i].normalsBuffer);
 			gl.vertexAttribPointer(aNormalLocation, 3, gl.FLOAT, false, 0, 0);
 
-			gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffersArray[i].indicesBuffer);
+			gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers[i].indicesBuffer);
 
-			gl.drawElements(gl.TRIANGLES, buffersArray[i].count, gl.UNSIGNED_SHORT, 0);
+			gl.drawElements(gl.TRIANGLES, buffers[i].count, gl.UNSIGNED_SHORT, 0);
 		}
 	};
 };
