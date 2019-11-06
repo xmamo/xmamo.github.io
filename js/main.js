7c4d36a9db7b18d6e0850a0c0705a1664ae11029
Matteo Morena <morena.matteo@gmail.com>
2019-11-06 13:53:53 +0100

    Move VanillaVotifier download link updater to main.js

--- js/main.js
+++ js/main.js
@@ -5,4 +5,19 @@
     while (elements.length > 0) {
         elements[0].classList.remove("js-only");
     }
+
+    elements = document.querySelectorAll("[data-github-download]");
+	for (var i = 0, count = elements.length; i < count; i++) {
+		(function (element) {
+			var httpRequest = new XMLHttpRequest();
+			httpRequest.onreadystatechange = function () {
+				if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
+					var response = JSON.parse(httpRequest.responseText);
+					element.href = response.assets[0].browser_download_url;
+				}
+			};
+			httpRequest.open("GET", "https://api.github.com/repos/" + element.getAttribute("data-github-download") + "/releases/latest");
+			httpRequest.send();
+		})(elements[i]);
+	}
 })();
