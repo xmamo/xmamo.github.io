6579ed08f056359983d9104612ca5c1895ce41bc
Mamo <morena.matteo@gmail.com>
2021-01-16 12:35:18 +0100

    Reformat js/main.js

--- js/main.js
+++ js/main.js
@@ -2,16 +2,23 @@
 
 (function () {
 	var elements = document.querySelectorAll("[data-github-download]");
-	for (var i = 0, count = elements.length; i < count; i++) {
+
+	for (var i = 0, count = elements.length; i < count; ++i) {
 		(function (element) {
 			var httpRequest = new XMLHttpRequest();
+
 			httpRequest.onreadystatechange = function () {
 				if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
 					var response = JSON.parse(httpRequest.responseText);
 					element.href = response.assets[0].browser_download_url;
 				}
 			};
-			httpRequest.open("GET", "https://api.github.com/repos/" + element.getAttribute("data-github-download") + "/releases/latest");
+
+			httpRequest.open(
+				"GET",
+				"https://api.github.com/repos/" + element.getAttribute("data-github-download") + "/releases/latest"
+			);
+
 			httpRequest.send();
 		})(elements[i]);
 	}
