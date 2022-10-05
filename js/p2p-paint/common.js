c4ab30fb0a77569da5f0acf25d7cfd2fc76d81a9
Matteo Morena <8678043+xmamo@users.noreply.github.com>
2022-10-05 11:29:05 +0200

    Delete P2P Paint

--- js/p2p-paint/common.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-var p2pPaint = p2pPaint || {};
-p2pPaint.canvas = [document.getElementById("p2p-paint-canvas-0"), document.getElementById("p2p-paint-canvas-1")];
-p2pPaint.context = [p2pPaint.canvas[0].getContext("2d"), p2pPaint.canvas[1].getContext("2d")];
